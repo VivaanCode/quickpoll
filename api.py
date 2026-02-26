fd75e3cc5cb8ef565007c509c91ec94c191f3ee1
Vivaan <87912804+VivaanCode@users.noreply.github.com>
2026-02-26 19:53:05 +0000

    finished api

--- api.py
+++ api.py
@@ -12,8 +12,33 @@ def createId():
     id = ""
     for i in range(6):
         id += random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+    if id in polls:
+        return createId()
     return id
 
 def createPoll(question, *args):
     print(question)
     print(args)
+    id = createId()
+
+    votes = [0] * len(args)
+
+    polls[id] = {
+        "question": question,
+        "options": args,
+        "votes": votes
+    }
+
+    return id
+
+def getPoll(id):
+    return polls.get(id, None)
+
+def vote(id, option):
+    poll = getPoll(id)
+    if poll:
+        if option in poll["options"]:
+            poll["votes"].index(option) += 1
+            return True
+    return False
+
